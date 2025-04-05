b83bd6ac78b25b4e5e23daa99b8db1120bffd2b2
Coy McNew <mcnewcp@gmail.com>
2025-04-05 15:25:45 -0500

    use shorthand for session_state

--- streamlit_app_from_scratch.py
+++ streamlit_app_from_scratch.py
@@ -1,25 +1,24 @@
 import streamlit as st
+from streamlit import session_state as ss
 from health_assistant import health_assistant
 from ui import stream_response
-from agents import Runner, Agent, ItemHelpers
-from openai.types.responses import ResponseTextDeltaEvent
 import asyncio
 
 st.title("Health Assistant `v0.01`")
 
 # Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if "messages" not in ss:
+    ss.messages = []
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
+for message in ss.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 # Accept user input
 if prompt := st.chat_input("Hello!  I'm your health assistant.  What's up?", key = "user_input"):
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    ss.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
